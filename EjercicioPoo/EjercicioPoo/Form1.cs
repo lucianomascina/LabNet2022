760166a9bb08c25a3f5d66b3cd294703fb58aaf4
lucianomascina <lucianomascina@gmail.com>
2022-09-14 23:27:39 -0300

    se agregó form mostrar

--- EjercicioPoo/EjercicioPoo/Form1.cs
+++ EjercicioPoo/EjercicioPoo/Form1.cs
@@ -27,17 +27,18 @@ namespace EjercicioPoo
 
         private void buttonGuardar_Click(object sender, EventArgs e)
         {
+            Transports.Add(new Bus(Convert.ToInt32(bus1.Value)));
+            Transports.Add(new Bus(Convert.ToInt32(bus2.Value)));
+            Transports.Add(new Bus(Convert.ToInt32(bus3.Value)));
+            Transports.Add(new Bus(Convert.ToInt32(bus4.Value)));
+            Transports.Add(new Bus(Convert.ToInt32(bus5.Value)));
+
             Transports.Add(new Taxi(Convert.ToInt32(taxi1.Value)));
             Transports.Add(new Taxi(Convert.ToInt32(taxi2.Value)));
             Transports.Add(new Taxi(Convert.ToInt32(taxi3.Value)));
             Transports.Add(new Taxi(Convert.ToInt32(taxi4.Value)));
             Transports.Add(new Taxi(Convert.ToInt32(taxi5.Value)));
 
-            Transports.Add(new Bus(Convert.ToInt32(bus1.Value)));
-            Transports.Add(new Bus(Convert.ToInt32(bus2.Value)));
-            Transports.Add(new Bus(Convert.ToInt32(bus3.Value)));
-            Transports.Add(new Bus(Convert.ToInt32(bus4.Value)));
-            Transports.Add(new Bus(Convert.ToInt32(bus5.Value)));
         }
 
         private void buttonMostrar_Click(object sender, EventArgs e)
--- EjercicioPoo/EjercicioPoo/FormShow.cs
+++ EjercicioPoo/EjercicioPoo/FormShow.cs
@@ -12,15 +12,29 @@ namespace EjercicioPoo
 {
     public partial class FormShow : Form
     {
+        List<PublicTransport> Transports = new List<PublicTransport>();
         public FormShow(List<PublicTransport> transports)
         {
             InitializeComponent();
-
+            this.Transports = transports;
         }
 
         private void FormShow_Load(object sender, EventArgs e)
         {
+            labelBus1.Text = $"{Transports[0].Passengers}  pasajeros";
+            labelBus2.Text = $"{Transports[1].Passengers}  pasajeros";
+            labelBus3.Text = $"{Transports[2].Passengers}  pasajeros";
+            labelBus4.Text = $"{Transports[3].Passengers}  pasajeros";
+            labelBus5.Text = $"{Transports[4].Passengers}  pasajeros";
+
+            labelTaxi1.Text = $"{Transports[5].Passengers}  pasajeros";
+            labelTaxi2.Text = $"{Transports[6].Passengers}  pasajeros";
+            labelTaxi3.Text = $"{Transports[7].Passengers}  pasajeros";
+            labelTaxi4.Text = $"{Transports[8].Passengers}  pasajeros";
+            labelTaxi5.Text = $"{Transports[9].Passengers}  pasajeros";
 
         }
+
+     
     }
 }
