6013a3d7485d406f5201c61c13e34f3ce12e9684
lucianomascina <lucianomascina@gmail.com>
2022-09-16 14:10:42 -0300

    se realizo el primer ejercicio

---
+++ PracticaExceptions/PracticaExceptions/FormMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaExceptions
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.ShowDialog();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PracticaExceptions/PracticaExceptions/MyExceptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExceptions
+{
+    public class MyExceptions
+    {
+        public static decimal Division(decimal n1, decimal n2)
+        {
+
+            try
+            {
+                
+                decimal result = n1 / n2;
+                return result;
+            }
+            catch(DivideByZeroException ex)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        
+
+    }
+}
