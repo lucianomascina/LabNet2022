a0ebc90567af6810ec65166ae7e30a0bea97f026
lucianomascina <lucianomascina@gmail.com>
2022-10-05 14:12:23 -0300

    se empezo crud categories

--- Lab.EF/Lab.EF.MVC/Controllers/CategoryController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/CategoryController.cs
@@ -36,5 +36,109 @@ namespace Lab.EF.MVC.Controllers
             }
 
         }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(CategoryView  categoryView)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Categories category = new Categories
+                    {
+                         CategoryID = categoryView.CategoryID,
+                         CategoryName = categoryView.CategoryName,
+                         Description = categoryView.Description,
+                         Picture = categoryView.Picture   
+                    };
+
+                    _categoriesLogic.Add(category);
+
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+        }
+
+        //[HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _categoriesLogic.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            try
+            {
+                var category = await _categoriesLogic.GetById(id);
+
+                var categoryView = new CategoryView
+                {
+                     CategoryID = category.CategoryID,
+                     CategoryName = category.CategoryName,
+                     Description = category.Description,
+                     Picture = category.Picture
+                };
+
+                return View(categoryView);
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CategoryView  categoryView)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Categories category = new Categories
+                    {
+                         CategoryID = categoryView.CategoryID,
+                         CategoryName = categoryView.CategoryName,
+                         Description = categoryView.Description,
+                         Picture = categoryView.Picture 
+                    };
+
+                    _categoriesLogic.Update(category);
+
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+        }
     }
 }
