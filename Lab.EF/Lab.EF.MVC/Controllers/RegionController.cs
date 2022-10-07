a6cdca3bd8bbfd4570194ef483c7d3bac91301c2
lucianomascina <lucianomascina@gmail.com>
2022-10-07 10:28:17 -0300

    se agrego getall regions ajax

--- Lab.EF/Lab.EF.MVC/Controllers/RegionController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/RegionController.cs
@@ -15,26 +15,25 @@ namespace Lab.EF.MVC.Controllers
     public class RegionController : Controller
     {
         private IService<Region> _regionService = new RegionService();
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            try
-            {
-                List<Region> regions = await _regionService.GetAll();
-
-                List<RegionView> regionsViews = regions.Select(r => new RegionView
-                {
-                    RegionID = r.RegionID,
-                    RegionDescription = r.RegionDescription  
-
-                }).ToList();
-
-                return View(regionsViews);
-            }
-            catch (Exception ex)
-            {
-                return RedirectToAction("Index", "Error");
-            }
-
+            return View();
         }
+
+        [HttpGet]
+        public async Task<JsonResult> GetAll()
+        {
+            List<Region> regions = await _regionService.GetAll();
+
+            var regionsViews = regions.Select(r => new RegionView
+            {
+                 RegionID = r.RegionID,
+                 RegionDescription = r.RegionDescription,
+
+            }).ToList();
+
+            return Json(regionsViews, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
